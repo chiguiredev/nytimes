d0c0ddc80f7d8378eff0a6505ccf005f9135412b
rafaelsalass <rafaelsalass20@gmail.com>
2019-03-04 12:46:21 -0400

    Updates

---
+++ precache-manifest.12ed2f283113553f6319553de402a2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "889800d51514249edafc",
+    "url": "/nytimes/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "889800d51514249edafc",
+    "url": "/nytimes/static/js/main.889800d5.chunk.js"
+  },
+  {
+    "revision": "d6202d8d81ccb6ede550",
+    "url": "/nytimes/static/js/runtime~main.d6202d8d.js"
+  },
+  {
+    "revision": "1de1ed297b5aa4764d28",
+    "url": "/nytimes/static/js/2.1de1ed29.chunk.js"
+  },
+  {
+    "revision": "ba7f1feeb884629ed4915da42c060f5a",
+    "url": "/nytimes/static/media/logo.ba7f1fee.svg"
+  },
+  {
+    "revision": "ae1566e097e630e3b7a8bc0ff158d793",
+    "url": "/nytimes/index.html"
+  }
+];
